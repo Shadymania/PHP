dcbcdbcd8e0fc31ca6f50b866a4c8f481ce433a1
Shadymania <shadymania88@gmail.com>
2020-07-27 13:12:33 +0545

    Include & Require Index Page

--- basicToNextLevel/includeRequire.php
+++ basicToNextLevel/includeRequire.php
@@ -1,14 +1,19 @@
-<?php 
+	<?php
+	echo "INCLUDE<br/>"; 
 	include ('includeRequireFile.php');
-	echo "End of PHP";
- ?>
 
+	echo "<hr/>REQUIRE<br/>";
+	require ('includeRequireFile.php');
+
+	echo "<br/>End of PHP";
+ 	?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Include & Require</title>
 </head>
 <body>
-	
+	<?php include 'includeRequireContent.php'; ?>
+
 </body>
 </html>
