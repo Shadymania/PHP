ca6280d7aa9bf0463fb2753f45dae40a9ccabc1f
Shadymania <shadymania88@gmail.com>
2020-07-28 16:40:12 +0545

    this is header of add.php file

---
+++ basicToNextLevel/header.php
@@ -0,0 +1,45 @@
+<head>
+	<title>Template Header-Footer</title>
+	<!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <style>
+    	
+
+    	.error{
+    		color: white;
+
+    	}
+
+    	label{
+    		font-size: 20px;
+    		color: white;
+    	}
+    	input{
+    		color: black;
+    		font-size: 20px;
+    		background: white !important;
+    	}
+    	.brand{
+    		color: orange !important;
+    		font-size: 20px;
+    	}
+
+    	.brand-text{
+    		color: white !important;
+    	}
+    </style>
+</head>
+<body class="red lighten-1">
+	<nav class="container z-depth-0">
+		<a href="#" class="brand-logo brand-text">Logo Here</a>
+		<ul id="nav-mobile" class="right hide-on-small-and-down">
+			<li><a href="#" class="btn brand z-depth-0">
+				Add
+			</a></li>
+
+			<li><a href="#" class="btn brand z-depth-0">
+				Next
+			</a></li>
+		</ul>
+	</nav>
+
